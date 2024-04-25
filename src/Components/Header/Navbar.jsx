8e584f4301fb5cbdf1e3c4a04559fd64f6a4cf2e
zafarbek2005 <153996622+zafarbek2005@users.noreply.github.com>
2024-04-25 12:02:55 +0500

    2

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -1,38 +1,46 @@
-import React from 'react'
-import {  NavLink } from 'react-router-dom';
-import logo from '../Header/img/Logo (1).svg'
-import "./Navbar.scss"
+import { AiOutlineMenu } from "react-icons/ai"; 
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from '../Header/img/Logo (1).svg';
+import './Navbar.scss';
+
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    if (menuOpen) {
+      setMenuOpen(false);
+    }
+  };
+
   return (
-  <>
-  <header>
-  <div className="navbar Conteiner">    
-    <div className="logo">
-        <NavLink to="/">
-        <img src= {logo} alt="" />
-        </NavLink>
-    </div>
-    
-    <div className="items">
-    <NavLink to="/">Home</NavLink>
-        <NavLink to="/about">About Us</NavLink>
-        <NavLink to="/Carerslink">Careers</NavLink>
-        <NavLink to="/Serveses">Services</NavLink>
-        <NavLink to="/Bloglink">Blog</NavLink>
-        <NavLink to="/Productslink">Products</NavLink>
-        <button>Clone project</button>
-    </div>
+    <>
+      <header>
+        <div className={`navbar ${menuOpen ? 'open' : ''}`}>
+          <div className="logo" onClick={toggleMenu}>
+            <NavLink to="/">
+              <img src={logo} alt="" />
+            </NavLink>
+          </div>
 
-  </div>
-  
-  
+          <div className="items">
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/about">About Us</NavLink>
+            <NavLink to="/Carerslink">Careers</NavLink>
+            <NavLink to="/Serveses">Services</NavLink>
+            <NavLink to="/Bloglink">Blog</NavLink>
+            <NavLink to="/Productslink">Products</NavLink>
+            <button> Clone project</button>
+            <button className='menu' onClick={toggleMenu}><AiOutlineMenu /></button>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
 
-  </header>
-  
-  </>
-
-
-  )
-}
-
-export default Navbar
+export default Navbar;
